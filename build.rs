1e3d6a9cc57990620c84164b07a336d2c3fc11f4
Srinath Kailasa <srinathkailasa@gmail.com>
2024-10-07 14:36:41 +0100

    Uncomment sleef build

--- build.rs
+++ build.rs
@@ -269,9 +269,9 @@ fn main() {
         build_umfpack(out_dir.clone());
     }
 
-    if std::env::var("CARGO_FEATURE_SLEEF").is_ok() {
-        build_sleef()
-    }
+    // if std::env::var("CARGO_FEATURE_SLEEF").is_ok() {
+    //     build_sleef()
+    // }
 
     println!("cargo:rerun-if-changed=metal/rlst_metal.m");
     println!("cargo:rerun-if-changed=sleef_interface/sleef_avx.c");
